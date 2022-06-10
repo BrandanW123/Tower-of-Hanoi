efe89027868d7a94d68544889d3bbb27d149b9a4
Brandan <78829162+BrandanW123@users.noreply.github.com>
2022-06-09 21:59:54 -0400

    Update TheTowerOfW.java

--- src/TheTowerOfW.java
+++ src/TheTowerOfW.java
@@ -1,17 +1,5 @@
 import java.util.*;
 
-/* * The Tower of Hanoi is a puzzle that consists of three rods and a number of disks of different sizes, which can slide onto any rod. 
- * The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical (Links to an external site.)
- * Links to an external site. shape.
-
-The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
-
-1)Only one disk can be moved at a time.
-2)Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack.
-3)No disk may be placed on top of a smaller disk.
-4)Random Number of Disk between 4 and 10
-*/
-
 // Notes 4 Move increment 
 
 public class TheTowerOfW {
